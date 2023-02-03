926431a4a9d9ee7d07a3dd451f4e771568d17cc7
UdayBaskar630 <68768804+UdayBaskar630@users.noreply.github.com>
2023-02-03 17:10:08 +0530

    Add files via upload

---
+++ Regex_Password_Validate.java
@@ -0,0 +1,24 @@
+package DXC;
+
+class Regex_Password_Validate 
+	{
+		public static boolean isValidPassword(String password) 
+		{
+			boolean flag = false;
+			String regex = "[A-Za-z]{8,10}[0-9]{4}";
+			if (password.matches(regex))
+			{
+				flag = true;
+			}
+			return flag;
+		}
+		public static void main(String[] args)
+		{
+			String password = "gftdrstr8645";
+			System.out.println("The password of the customer is " + password);
+			if (isValidPassword(password))
+				System.out.println("The password is valid!");
+			else
+				System.out.println("The password is not valid!");
+		}
+	}
